89dda157e8027bce1598cd6ad610604113eaabc7
Exybore <exybore@becauseofprog.fr>
2020-03-14 23:25:13 +0100

    Added files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          src="/img/logo.png"
+          class="my-3"
+          transition="scale-transition"
+          height="200"
+          contain/>
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Highest Template
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          This is a front-end template including : Vue.js, Vuetify, Router, PWA
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: 'Home',
+  data () {
+    return {
+
+    }
   }
 }
 </script>
